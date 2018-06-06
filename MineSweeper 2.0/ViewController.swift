c0b7fb84632dd86d4e841a0ab8c94366deab4afa
Jack Zhu <momendie@yuchens-macbook-pro.local>
2018-06-06 01:16:49 -0400

    Initial Commit

---
+++ MineSweeper 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MineSweeper 2.0
+//
+//  Created by Yuchen Zhu on 2018-06-06.
+//  Copyright © 2018 Momendie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
